be7ce747d7af9d1265fa221fd42f35d2f9eca4aa
root <root@DESKTOP-STGEASR>
2024-07-07 13:49:58 +0800

    增加了 /算法 二叉树的性质

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+template<typename Type>
+struct TreeNode {
+    Type value;
+    TreeNode* lchild, *rchild;
+};
+
+template<typename Type>
+class BinaryTree {
+private:
+    TreeNode<Type> *root;
+public:
+    // explicit 无法进行隐式转换
+    explicit BinaryTree(Type val) {
+        root->value = val;
+    }
+
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
 }
